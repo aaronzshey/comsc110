7ff8f97a8d9beee2033fc97e252bb9c588f64f50
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2022-06-15 09:14:05 -0700

    formating

--- programTemplate.cpp
+++ programTemplate.cpp
@@ -1,37 +1,37 @@
-//Objective:  template for C++ programs and to test code
-//Name: Aaron Shey 2027142
-//Course: COMSC-110-5003
-//Compiler: gcc - GNU C and C++ compiler
-//Editor: mousepad - text editor for Xfce 
+// Objective:  template for C++ programs and to test code
+// Name: Aaron Shey 2027142
+// Course: COMSC-110-5003
+// Compiler: g++ - GNU C and C++ compiler
+// Editor: mousepad - text editor for Xfce
 
-//libraries
+// libraries
 #include <iostream>
 using namespace std;
 
-//Programmer defined data types
-//NONE
+// Programmer defined data types
+// NONE
 
-//Special compiler dependent definitions
-//NONE
+// Special compiler dependent definitions
+// NONE
 
-//global constants/variables
-//NONE
+// global constants/variables
+// NONE
 
-//Programmer defined functions
-//NONE
+// Programmer defined functions
+// NONE
 
-//main program
-int main()
-{
-  //Data
-  //NONE
+// main program
+int main() {
+  // Data
+  // NONE
 
   // introduction
-  cout << "Objective: This program will serve as a template for all programs\n written in this course.\n"; 
-  cout << "Programmer: Aaron\n"; 
-  cout << "Editor(s) used: mousepad\n"; 
-  cout << "Compiler(s) used: gcc\n"; 
-  cout << "File: " << __FILE__ << endl; 
-  cout << "Complied: " << __DATE__ << " at " << __TIME__ << endl << endl; 
+  cout << "Objective: This program will serve as a template for all programs\n "
+          "written in this course.\n";
+  cout << "Programmer: Aaron\n";
+  cout << "Editor(s) used: mousepad\n";
+  cout << "Compiler(s) used: g++\n";
+  cout << "File: " << __FILE__ << endl;
+  cout << "Complied: " << __DATE__ << " at " << __TIME__ << endl << endl;
 
-}//main
+}  // main
