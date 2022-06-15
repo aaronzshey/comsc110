6565d2cf78f5257b765791710a32f621df9346d3
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2022-06-15 09:11:30 -0700

    added template

--- hello.cpp
+++ hello.cpp
@@ -3,4 +3,5 @@ using namespace std;
 
 int main() {
   cout << "asdfasdf World!";
+  return 0;
 }
---
+++ programTemplate.cpp
@@ -0,0 +1,37 @@
+//Objective:  template for C++ programs and to test code
+//Name: Aaron Shey 2027142
+//Course: COMSC-110-5003
+//Compiler: gcc - GNU C and C++ compiler
+//Editor: mousepad - text editor for Xfce 
+
+//libraries
+#include <iostream>
+using namespace std;
+
+//Programmer defined data types
+//NONE
+
+//Special compiler dependent definitions
+//NONE
+
+//global constants/variables
+//NONE
+
+//Programmer defined functions
+//NONE
+
+//main program
+int main()
+{
+  //Data
+  //NONE
+
+  // introduction
+  cout << "Objective: This program will serve as a template for all programs\n written in this course.\n"; 
+  cout << "Programmer: Aaron\n"; 
+  cout << "Editor(s) used: mousepad\n"; 
+  cout << "Compiler(s) used: gcc\n"; 
+  cout << "File: " << __FILE__ << endl; 
+  cout << "Complied: " << __DATE__ << " at " << __TIME__ << endl << endl; 
+
+}//main
