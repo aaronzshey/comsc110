022f375a5efb5bafaa213709739703f9b4aabf37
CarlyRaeJepsenStan <44917034+CarlyRaeJepsenStan@users.noreply.github.com>
2022-06-16 11:49:13 -0700

    create exer3

---
+++ aboutMe.cpp
@@ -0,0 +1,39 @@
+// Objective:  template for C++ programs and to test code
+// Name: Aaron Shey 2027142
+// Course: COMSC-110-5003
+// Compiler: g++ - GNU C and C++ compiler
+// Editor: mousepad - text editor for Xfce
+
+// libraries
+#include <iostream>
+using namespace std;
+
+// Programmer defined data types
+// NONE
+
+// Special compiler dependent definitions
+// NONE
+
+// global constants/variables
+// NONE
+
+// Programmer defined functions
+// NONE
+
+// main program
+int main() {
+  // Data
+  int luckyNumber;  //my favorite number
+  float amountEarned;  //how much I want to save
+  string movie;  //my favorite movie
+  char initial;  //my first initial
+
+  // introduction
+  cout << "Objective: This program will serve as a template for all programs\n written in this course.\n";
+  cout << "Programmer: Aaron\n";
+  cout << "Editor(s) used: mousepad\n";
+  cout << "Compiler(s) used: g++\n";
+  cout << "File: " << __FILE__ << endl;
+  cout << "Complied: " << __DATE__ << " at " << __TIME__ << endl << endl;
+
+}  // main
