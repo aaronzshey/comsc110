7002dcafa154780745cd09fb1f3e6dd838ad052e
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2022-06-20 09:08:37 -0700

    some independent exercises

---
+++ lighthouse1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+#include <cmath>
+
+int main() {
+	cout.setf(ios::fixed);
+	cout << setprecision(0);
+	int lighthouseHeightinFeet;
+	cout << "Type in the height of your lighthouse in feet and press enter. ";
+	cin >> lighthouseHeightinFeet;
+	cin.ignore(256, '\n');
+	double distInMiles = sqrt(double(0.8 * lighthouseHeightinFeet));
+	cout << "a " << lighthouseHeightinFeet << " foot tall lighthouse can be seen from " << distInMiles << " feet away.";
+}
